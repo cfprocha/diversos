f663ad8db3a7c915acf1d8fd97eeeea3232d7e22
Carlos F Paleo da Rocha <79323744+cfprocha@users.noreply.github.com>
2022-06-29 11:48:20 -0300

    Update index.php
    Exclusão de linha desnecessária

--- cadastro/index.php
+++ cadastro/index.php
@@ -11,7 +11,6 @@ include("bd/conecta.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista Clientes</title>
     <link rel="stylesheet" href="css/style.css">
-    <script src="js/clientes.js"></script>
 </head>
 <body>
     <h1>Cadastro de Clientes</h1>
